327c4927c712aa7111df9c1ca43b4460beb04124
jjanagan407 <jjanagan407@gmail.com>
2026-07-24 16:36:41 +0530

    added recursion.java

---
+++ recursion.java
@@ -0,0 +1,18 @@
+public class recursion {
+
+    public static int factorial(int n) {
+
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+
+        int result = factorial(5);
+
+        System.out.println(result);
+    }
+}
